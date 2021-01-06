01d4cab8baa79bb58f27d0a79a7b49c4214c6342
seisen <jochim6.2@hotmail.fr>
2021-01-06 14:49:38 +0100

    major update

--- models/location.model.php
+++ models/location.model.php
@@ -1,1 +1,18 @@
 <?php
+require("utils.connexionBDD.php");
+/**
+ * @param $car_id
+ * @param $user_id
+ * @param $duration " in days "
+ */
+function addLocation($car_id, $user_id, $start_date, $end_date){
+
+    $stmt = connect() ->prepare("INSERT INTO locations (car_id, user_id,start_date,end_date) VALUES (:car_id, :user_id,:start_date,:end_date)");
+    $stmt->bindParam(':car_id', $car_id);
+    $stmt->bindParam(':user_id', $user_id);
+    $stmt->bindParam(':start_date',$start_date );
+    $stmt -> bindParam(':end_date', $end_date);
+    $stmt->execute();
+    $stmt -> closeCursor();
+
+}
--- views/car.view.php
+++ views/car.view.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="shortcut icon" type="image/ico" href="img/favicon.gif" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title> Voiture </title>
+</head>
+<body>
+
 <?php
+    echo '<h1>'.$_POST["name"].'</h1>';
+    ?>
+    <ul>
+        <?php
+            echo '<li>'.$_POST["color"].'</li>>';
+            echo '<li>'.$_POST["mark"].'</li>>';
+            echo '<li>'.$_POST["price"].'</li>>';
+        ?>
+    </ul>
+
+<form method="post" action="../views/location.add.view.php">
+    <?php echo'<input name="car_id" value="'.$_POST["car_id"].'" type="hidden">'; ?>
+    <input value="Louer" type="submit">
+</form>
+
+</body>
+</html>
